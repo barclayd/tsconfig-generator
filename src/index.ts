0e587028f57397b59890834cd61be85e02cfc75b
Daniel Barclay <barclaysd@me.com>
2020-12-29 11:02:14 +0000

    Add shelljs, run additional setup for Node

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import { copyFileSync } from 'fs';
+import { Answer, Framework } from './types';
+import shell from 'shelljs';
+import * as inquirer from 'inquirer';
 
-const inquirer = require('inquirer');
-
-enum Framework {
-  React = 'React',
-  Next = 'Next',
-  Node = 'Node',
-  Electron = 'Electron',
-}
-
-interface Answer {
-  framework: Framework;
-}
-
-const FILENAME = 'tsconfig.json';
+const setupScriptMap = new Map<Framework, string>([
+  [Framework.Node, './scripts/node.sh'],
+]);
 
 const pathToFrameworkTSConfig = async (
   framework: Framework,
 ): Promise<string> => {
-  return `./templates/${framework.toLowerCase()}-tsconfig.json`;
+  return `./src/templates/${framework.toLowerCase()}-tsconfig.json`;
 };
 
 const writeTSConfig = async (framework: Framework) => {
   const cwd = process.cwd();
   const tsconfigPath = await pathToFrameworkTSConfig(framework);
-  copyFileSync(tsconfigPath, cwd + `/${FILENAME}`);
+  copyFileSync(tsconfigPath, cwd + `/tsconfig.json`);
+};
+
+const additionalSetup = (framework: Framework) => {
+  const setupScript = setupScriptMap.get(framework);
+  if (!setupScript) {
+    return;
+  }
+  shell.exec(setupScript);
 };
 
 (async () => {
@@ -38,6 +38,7 @@ const writeTSConfig = async (framework: Framework) => {
       },
     ]);
     await writeTSConfig(framework);
+    additionalSetup(framework);
   } catch (error) {
     console.log(`Error occurred: ${error}`);
   }
