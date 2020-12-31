a43458d7f0ed34d2fb56aaa468665740a4d1b8d8
Daniel Barclay <barclaysd@me.com>
2020-12-31 19:39:00 +0000

    More flexible productionPackageJson creation to reduce build errors on first publish

--- scripts/postBuild.sh
+++ scripts/postBuild.sh
@@ -6,6 +6,6 @@ mv ./dist/templates/npx/package.json ./dist/templates/npx/old-package.json
 mv ./dist/templates/npx/.gitignore ./dist/templates/npx/gitignore
 touch ./dist/templates/npx/README.md
 mkdir ./dist/templates/npx/src && touch ./dist/templates/npx/src/index.ts
-cp -r ./src/scripts ./dist/scripts
+cp -r ./src/scripts ./dist/scripts &>/dev/null
 cp ./PACKAGE_README.md ./dist/README.md
 ts-node ./scripts/productionPackageJson.ts
