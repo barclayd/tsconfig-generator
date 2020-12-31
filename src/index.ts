afc0806cfa3eca002fb287ce305bfa5345e054e7
Daniel Barclay <barclaysd@me.com>
2020-12-31 12:40:43 +0000

    Support module-alias

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
+import moduleAlias from 'module-alias';
+if (process.env.NODE_ENV === 'production') {
+  moduleAlias();
+}
 import { FrameworkAnswer, Framework } from './types';
 import { isPackageJsonPresent, generatePackageJson } from './helpers';
 import * as inquirer from 'inquirer';
 import { TSConfigService } from '@/services/TSConfigService';
 import { ScriptSetupService } from '@/services/ScriptSetupService';
 import { NpxService } from '@/services/NpxService';
+import path from 'path';
+
+export const pathForFolder = (folder: string) =>
+  path.resolve(__dirname, folder);
 
 (async () => {
   try {
