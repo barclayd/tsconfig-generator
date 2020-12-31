e283f8bab49148de4e91f72dfdae66a8bb9b22ac
Daniel Barclay <barclaysd@me.com>
2020-12-31 19:44:34 +0000

    Add #!/usr/bin/env node for npx package

--- scripts/postBuild.sh
+++ scripts/postBuild.sh
@@ -5,7 +5,12 @@ rsync -a . ./dist/templates/npx --exclude .idea --exclude node_modules --exclude
 mv ./dist/templates/npx/package.json ./dist/templates/npx/old-package.json
 mv ./dist/templates/npx/.gitignore ./dist/templates/npx/gitignore
 touch ./dist/templates/npx/README.md
-mkdir ./dist/templates/npx/src && touch ./dist/templates/npx/src/index.ts
+mkdir ./dist/templates/npx/src
+touch ./dist/templates/npx/src/index.ts
+{
+  echo "#!/usr/bin/env node"
+} >> ./dist/templates/npx/src/index.ts
+touch ./dist/templates/npx/src/index.ts
 cp -r ./src/scripts ./dist/scripts &>/dev/null
 cp ./PACKAGE_README.md ./dist/README.md
 ts-node ./scripts/productionPackageJson.ts
